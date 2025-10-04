c7d46860c3b03be0add5566667d71b0d8fe290f5
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-04 22:02:28 +0200

    removed deprecated code

--- sear/irrsmo00/xml_parser.hpp
+++ sear/irrsmo00/xml_parser.hpp
@@ -12,17 +12,7 @@ namespace SEAR {
 // XMLParser Parses an XML String and forms a JSON String
 class XMLParser {
  private:
-  void parseXMLTags(nlohmann::json& input_json, std::string input_xml_string);
-  void parseXMLData(nlohmann::json& input_json,
-                    const std::string& data_within_outer_tags,
-                    const std::string& outer_tag);
   void XMLToJSON(nlohmann::json& input_json, std::string xml_string);
-  static void updateJSON(nlohmann::json& input_json, nlohmann::json& inner_data,
-                         std::string outer_tag);
-  static std::string replaceXMLChars(std::string xml_data);
-  static std::string replaceSubstring(std::string data, std::string& substring,
-                                      std::string replacement,
-                                      std::size_t start);
 
  public:
   nlohmann::json buildJSONString(SecurityRequest& request);
