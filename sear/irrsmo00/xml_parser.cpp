39cc513a8718a9411d1297555d2c3be8d6dcf2fa
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-04 18:08:05 +0200

    test

--- sear/irrsmo00/xml_parser.cpp
+++ sear/irrsmo00/xml_parser.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <regex>
 #include <string>
+#include <vector>
 
 #include "../conversion.hpp"
 #include "logger.hpp"
@@ -80,7 +81,9 @@ void XMLParser::XMLToJSON(nlohmann::json& input_json, std::string xml_string) {
   rapidxml::xml_document<> doc;
   rapidxml::xml_node<> * root_node;
 
-  doc.parse<0>(xml_string.data()); 
+  vector<char> xml_copy(xml_string.begin(), xml_string.end());
+
+  doc.parse<0>(&xml_copy[0]); 
 
   // Find our root node
 	root_node = doc.first_node("securityresult");
