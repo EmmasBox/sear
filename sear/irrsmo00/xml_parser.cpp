424d2f38a3a306d2e097464879e7b29f31af73b7
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-04 19:23:59 +0200

    test

--- sear/irrsmo00/xml_parser.cpp
+++ sear/irrsmo00/xml_parser.cpp
@@ -77,11 +77,15 @@ void XMLParser::XMLToJSON(nlohmann::json& input_json, std::string xml_string) {
 
   // Find our root node
 	root_node = doc.first_node("securityresult");
-
+   Logger::getInstance().debug("test1");
 	for (rapidxml::xml_node<> * result_node = root_node->first_node(); result_node; result_node = result_node->next_sibling())
 	{
+    
+    Logger::getInstance().debug("test2");
     for(rapidxml::xml_node<> * admin_type_node = result_node->first_node("command"); admin_type_node; admin_type_node = admin_type_node->next_sibling())
     {
+       Logger::getInstance().debug("saf", result_node->first_attribute("safreturncode")->value());
+      Logger::getInstance().debug("Decoded result XML:", xml_buffer);
       input_json["saf_return_code"] = result_node->first_attribute("safreturncode")->value();
       input_json["racf_return_code"] = result_node->first_attribute("returncode")->value();
       input_json["reason_code"] = result_node->first_attribute("reasoncode")->value();
