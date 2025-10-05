de09143831a8e349fd490b9939a0ae60d30fe1a8
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-10-06 00:20:57 +0200

    fixed argument having two different names

--- sear/irrsmo00/xml_parser.hpp
+++ sear/irrsmo00/xml_parser.hpp
@@ -12,7 +12,7 @@ namespace SEAR {
 // XMLParser Parses an XML String and forms a JSON String
 class XMLParser {
  private:
-  void XMLToJSON( std::string xml_string, nlohmann::json& input_json, SecurityRequest& request);
+  void XMLToJSON( std::string xml_string, nlohmann::json& output_json, SecurityRequest& request);
 
  public:
   nlohmann::json buildJSONString(SecurityRequest& request);
