c01de46513436de0591c0504b18a84fce3dced8a
Emma Skovgaard <64806882+EmmasBox@users.noreply.github.com>
2025-11-01 23:06:20 +0100

    added IRRSIM function codes

--- sear/irrsim00/irrsim00.hpp
+++ sear/irrsim00/irrsim00.hpp
@@ -4,6 +4,16 @@
 #include "sear_result.h"
 #include "security_request.hpp"
 
+#include <cstdint>
+
+const uint16_t RACF_TO_NDS_FUNCTION_CODE     = 0x0003;
+const uint16_t NDS_TO_RACF_FUNCTION_CODE     = 0x0004;
+const uint16_t RACF_TO_KERB_FUNCTION_CODE    = 0x0005;
+const uint16_t KERB_TO_RACF_FUNCTION_CODE    = 0x0006;
+const uint16_t DID_TO_RACF_FUNCTION_CODE     = 0x0008;
+const uint16_t USER_TO_EMAIL_FUNCTION_CODE   = 0x0009;
+const uint16_t EMAIL_TO_USER_FUCTION_CODE    = 0x000A;
+
 /* Prototype for IRRSIM00 */
 extern "C" {
 void IRRSIM00(char *,               // Workarea
