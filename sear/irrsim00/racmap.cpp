0672eae2c56994828a6ed52a446f4ea5037a1d70
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-11-29 16:13:59 +0100

    Initial work on racmap support

--- sear/irrsim00/racmap.cpp
+++ sear/irrsim00/racmap.cpp
@@ -5,8 +5,14 @@
 #include "zoslib.h"
 #endif
 
+#include <nlohmann/json.hpp>
+
 #include "irrsim00.hpp"
+#include "security_reqeust.hpp"
 
 namespace SEAR {
 
+void Racmap::extract(SecurityRequest &request) {
+
+}
 } // namespace SEAR
--- sear/irrsim00/racmap.hpp
+++ sear/irrsim00/racmap.hpp
@@ -2,9 +2,14 @@
 #define __RACMAP_H_
 
 #include <string>
+#include "irrsim00.hpp"
+#include "security_request.hpp"
 
 namespace SEAR {
-    
+class Racmap {
+    public:
+    static void extract(SecurityRequest &request);
+};
 }  // namespace SEAR
 
 #endif
