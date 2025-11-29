20911cb4e663a0596e6060f82eacf6b241c8f2c0
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-11-29 16:06:47 +0100

    Initial work on RACMAP

---
+++ sear/irrsim00/racmap.cpp
@@ -0,0 +1,12 @@
+
+#ifdef __TOS_390__
+#include <unistd.h>
+#else
+#include "zoslib.h"
+#endif
+
+#include "irrsim00.hpp"
+
+namespace SEAR {
+
+} // namespace SEAR
---
+++ sear/irrsim00/racmap.hpp
@@ -0,0 +1,10 @@
+#ifndef __RACMAP_H_
+#define __RACMAP_H_
+
+#include <string>
+
+namespace SEAR {
+    
+}  // namespace SEAR
+
+#endif
