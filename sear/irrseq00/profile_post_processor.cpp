3dba4e601f531c1f553ce05ac57ea0581e74af05
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-12-08 13:43:45 +0100

    test

--- sear/irrseq00/profile_post_processor.cpp
+++ sear/irrseq00/profile_post_processor.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cctype>
+#include <cstddef>
 #include <cstdio>
 #include <cstring>
 #include <memory>
@@ -233,7 +234,7 @@ void ProfilePostProcessor::postProcessRACFRRSF(SecurityRequest &request) {
   // Set settings
   const racf_rrsf_set_settings_t *command_redirection_settings =
       reinterpret_cast<const racf_rrsf_set_settings_t *>(
-          p_profile + sizeof(racf_rrsf_extract_results_t));
+          p_profile + offsetof(racf_rrsf_extract_results_t, rrsf_extract_result->automatic_command_redirection) + sizeof(racf_rrsf_set_settings_t));
 
   const racf_rrsf_set_settings_t *password_redirection_settings =
       reinterpret_cast<const racf_rrsf_set_settings_t *>(rrsf_extract_result->automatic_password_redirection);
