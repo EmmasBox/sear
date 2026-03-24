7fb9927ff3a8da83c3c00999e3d40b2c20a83d2b
Emma Skovgård <64806882+EmmasBox@users.noreply.github.com>
2026-03-24 21:56:40 +0100

    test

--- sear/irrseq00/profile_extractor.hpp
+++ sear/irrseq00/profile_extractor.hpp
@@ -30,7 +30,7 @@ class ProfileExtractor : public Extractor {
 struct DefaultDeleter {
   void operator()(void *ptr) const {
     Logger::getInstance().debugFree(ptr);
-    delete ptr;
+    delete* ptr;
     Logger::getInstance().debug("Done");
   }
 };
